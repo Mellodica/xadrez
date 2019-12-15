6efe650086ea03a7fc78b06a39d41c73e07661bf
Helber Oliveira <mellodica.louise@gmail.com>
2019-12-14 22:25:18 -0300

    Classes: ColocarPecas, Rei, Torre

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez
 {
@@ -12,6 +13,10 @@ namespace xadrez
         static void Main(string[] args)
         {
             Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+
+            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+            tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tabuleiro);
 
--- xadrez/Tabuleiro/Peca.cs
+++ xadrez/Tabuleiro/Peca.cs
@@ -15,9 +15,9 @@ namespace tabuleiro
         public Tabuleiro tab { get; protected set; }
 
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimento = 0;
